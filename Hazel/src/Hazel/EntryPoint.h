701cf546ce56f053961e7decd5d773f0803bcb7a
sara_ismail <sara3d.es@gmail.com>
2024-08-08 16:19:42 +0400

    Premake lec6 all done
    Premake lec6 all done

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -14,6 +14,9 @@ int main(int argc, char** arv)
 	int a = 5;
 	float b = 3.33;
 	HZ_INFO("hi inttt {0} , float {1}", a, b);
+	HZ_TRACE("trace whiteeeeeeee");
+	HZ_ERROR("errrorrr");
+	
 	auto app = Hazel::CreateApplication();
 	app->Run();
 	delete app;
