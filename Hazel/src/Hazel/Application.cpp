257f038c00c4ebaf9007160f45a44e3a53565b56
sara_ismail <sara3d.es@gmail.com>
2024-08-16 15:29:08 +0400

    WindowAbstraction lec11 DONE
    WindowAbstraction lec11 DONE

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -3,35 +3,27 @@
 
 #include "Events/ApplicationEvent.h"
 #include "Hazel/Log.h"
-
+#include <GLFW/glfw3.h>
 
 namespace Hazel
 {
 	Application::Application()
-	{}
+	{
+		m_Window = std::unique_ptr<Window>(Window::Create());
+	}
 
 	Application::~Application()
 	{}
-
 	void Application::Run()
 	{
-		WindowResizeEvent e(1280, 720);
-
-		if (e.IsInCategory(EventCategoryApplication))
+		while (m_Running)
 		{
-			HZ_TRACE(e.GetWidth());
-			HZ_TRACE("EventCategoryApplication");
-		}
-		if (e.IsInCategory(EventCategoryInput))
-		{
-			HZ_TRACE(e.GetHeight());
-			HZ_TRACE("EventCategoryInput");
+			glClearColor(1, 0, 1, 1);
+			glClear(GL_COLOR_BUFFER_BIT);
+			m_Window->OnUpdate();
 		}
 
-		while (true)
-		{
-
-		}
+		while (true);
 	}
 
 }
--- Hazel/src/Hazel/Core.h
+++ Hazel/src/Hazel/Core.h
@@ -8,9 +8,17 @@
 		#define HAZEL_API __declspec(dllimport)
 #endif
 
-#define BIT(x) (1 << x)
 
 #else 
 	#error Hazel Only Support Windows
 #endif
 
+#ifdef HZ_ENABLE_ASSERTS
+#define HZ_ASSERT(x, ...) { if(!(x)) { HZ_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+#define HZ_CORE_ASSERT(x, ...) { if(!(x)) { HZ_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+#else
+#define HZ_ASSERT(x, ...)
+#define HZ_CORE_ASSERT(x, ...)
+#endif
+
+#define BIT(x) (1 << x)
